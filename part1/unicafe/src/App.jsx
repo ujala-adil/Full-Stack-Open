40d9195a6887542a889241a0eefa425b07d785d9
Ujala <ujala.spur@gmail.com>
2025-04-16 23:37:10 +0300

    Ex: 1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,18 +6,32 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  return (
+    <div>
+      <Heading title='give feedback' />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <Heading title='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Heading = ({ title }) => <h1>{title}</h1>
+
+const Display = ({ rating, count }) => <p>{rating} {count}</p>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total;
   const positive = total === 0 ? 0 : (good / total) * 100;
 
   return (
-    <div>
-      <Heading title='give feedback'/>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
-
-      <Heading title='statistics'/>
+    <>
       <Display rating="good" count={good} />
       <Display rating="neutral" count={neutral} />
       <Display rating="bad" count={bad} />
@@ -25,14 +39,9 @@ const App = () => {
       <p>all {total}</p>
       <p>average {average}</p>
       <p>positive {positive}%</p>
-    </div>
+    </>
   )
 }
 
-const Heading = ({title}) => <h1>{title}</h1>
-
-const Display = ({rating, count}) => <p>{rating} {count}</p>
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 export default App
