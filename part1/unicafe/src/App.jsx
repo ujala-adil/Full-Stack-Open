a4e7023d08f8e4efade3a0a0740ea46e4d73e8e7
Ujala <ujala.spur@gmail.com>
2025-04-16 23:38:16 +0300

    Ex: 1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -30,6 +30,10 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = total === 0 ? 0 : (good - bad) / total;
   const positive = total === 0 ? 0 : (good / total) * 100;
 
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
   return (
     <>
       <Display rating="good" count={good} />
