45817f5e1e9a15f7c51557ff0eb1cd8e0663de45
Ujala Adil <ujadil99@gmail.com>
2025-04-18 18:49:21 +0300

    Ex: 2.2: Course information step 7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,39 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+    <Part part={props.parts[3]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p><b>total of {props.total} exercises</b></p>
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises +
+          course.parts[3].exercises
+        }
+      />
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
     id: 1,
@@ -17,36 +53,16 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
   return <Course course={course} />
 }
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
 
 export default App
