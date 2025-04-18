320b7a9588076fc1975f9cf5278a2d02b35b8f85
Ujala Adil <ujadil99@gmail.com>
2025-04-18 19:09:20 +0300

    Ex: 2.3: Course information step 8

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,17 +18,19 @@ const Part = (props) => (
 const Total = (props) => <p><b>total of {props.total} exercises</b></p>
 
 const Course = ({course}) => {
+
+  // var sum = course.parts.reduce((sum, part)=>{
+  //   console.log(sum, part.name)
+  //   return sum + part.exercises
+  // }, 0)
+  // console.log(sum)
+
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises +
-          course.parts[3].exercises
-        }
+        total={course.parts.reduce((sum, part) => sum + part.exercises, 0)}
       />
     </div>
   )
