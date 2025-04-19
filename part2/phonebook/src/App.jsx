3a006a2444ab4279ba54864d2a2bf69710b67a83
Ujala <ujala.spur@gmail.com>
2025-04-19 16:09:49 +0300

    Ex: 2.6: The Phonebook Step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleOnChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    // setPersons(persons.concat(newName))
+    setPersons(persons.concat({ name: newName })) //need to wrap it in an object
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleOnChange} />
+        </div>
+        <div>debug: {newName}</div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {/* {persons.map((person, index) => {
+          return <li key={index}>{person.name}</li>
+        })} */}
+
+        {persons.map((person, index) => (
+            <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
