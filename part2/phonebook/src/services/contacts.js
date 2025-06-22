b883f996ddf12167c50e865b0e5a2133df038a8c
Ujala Adil <ujadil99@gmail.com>
2025-06-23 02:13:32 +0300

    Ex: 2.15*: The Phonebook step 10

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 const create = newPerson => {
-  const request = axios.post(baseUrl, newPerson)
-  return request.then(response => response.data)
+    const request = axios.post(baseUrl, newPerson)
+    return request.then(response => response.data)
 }
 
 const deletePerson = personId => {
@@ -16,5 +16,10 @@ const deletePerson = personId => {
     return request.then(response => response.data)
 }
 
+const updateNumber = (id, personObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, personObject)
+    return request.then(response => response.data)
+}
 
-export default { getAll, create, deletePerson }
+
+export default { getAll, create, deletePerson, updateNumber }
