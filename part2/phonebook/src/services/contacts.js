0411ca54fc9ac3864dba26c48ab583f64741caa6
Ujala <ujala.spur@gmail.com>
2025-06-23 00:22:26 +0300

    Ex: 2.14: The Phonebook step 9

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -11,5 +11,10 @@ const create = newPerson => {
   return request.then(response => response.data)
 }
 
+const deletePerson = personId => {
+    const request = axios.delete(`${baseUrl}/${personId}`)
+    return request.then(response => response.data)
+}
 
-export default { getAll, create }
+
+export default { getAll, create, deletePerson }
