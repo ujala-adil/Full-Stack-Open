3e4df01718b4624eb57df1a5b7cf50f0950fc9ce
Ujala Adil <ujadil99@gmail.com>
2025-06-26 02:42:24 +0300

    Ex: 3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
-var morgan = require('morgan')
 
 app.use(express.json())
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
