66893d649c26b0ca9f43f73aa41b940fa4570565
Ujala Adil <ujadil99@gmail.com>
2025-06-28 02:47:22 +0300

    Ex: 3.9: Phonebook backend step 9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,8 +9,13 @@ app.use(express.json())
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+const cors = require('cors')
+app.use(cors())
+
+
 
 let persons = [
   {
