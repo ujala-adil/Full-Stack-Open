2882f94a8e712b17e6b29172dfa9d39e010f3d21
Ujala Adil <ujadil99@gmail.com>
2025-07-02 23:48:40 +0300

    Ex: 3.12 Command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://ujala123:${password}@cluster0.sc1xqkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+//const url = `mongodb+srv://ujala123:${password}@cluster0.sc1xqkc.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if (process.argv.length === 3) {
+    Person.find({}).then(persons => {
+        console.log(`phonebook:`)
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        })
+        mongoose.connection.close()
+    })
+
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+    person.save().then(result => {
+        console.log('person contact saved!')
+        mongoose.connection.close()
+    })
+
+} else {
+    console.log('Invalid number of arguments.\nPlease input as:\n  node mongo.js <password> [name number]');
+    mongoose.connection.close();
+}
+
+
+
+
+
