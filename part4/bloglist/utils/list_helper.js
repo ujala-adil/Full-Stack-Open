e975c4b9d19516834113d69f1e14f596c84399a4
Ujala Adil <ujadil99@gmail.com>
2025-07-06 21:09:03 +0300

    Ex: 4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,26 @@
+const logger = require('./logger.js')
+
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0;
+  logger.info(blogs)
+  if (blogs.length === 1) {
+    sum = blogs[0].likes
+  }
+  else if (blogs.length > 1) {
+    const reducer = (total, blog) => {
+      return total + blog.likes
+    }
+    sum = blogs.reduce(reducer, 0)
+  }
+
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
