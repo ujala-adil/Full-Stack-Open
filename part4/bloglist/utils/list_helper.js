293eeb96da09806e699ee06fbd4f6192eb087ae1
Ujala Adil <ujadil99@gmail.com>
2025-07-07 03:06:58 +0300

    Ex: 4.6*: Helper Functions and Unit Tests, step 4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,5 @@
 const logger = require('./logger.js')
-
+const _ = require('lodash');
 
 const dummy = (blogs) => {
   return 1
@@ -7,7 +7,7 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   let sum = 0;
-  logger.info(blogs)
+  // logger.info(blogs)
 
   if (blogs.length === 1) {
     sum = blogs[0].likes
@@ -28,6 +28,27 @@ const favoriteBlog = (blogs) => {
   })
 }
 
+const mostBlogs = (blogs) => {
+  const blogCounts = _.countBy(blogs, 'author') //Loops through the blogs array and counts how many times each author appears.
+  //   output of countby: It's a simple object where the keys are the author names and the values are the number of blogs.
+  //{
+  //   Robert C. Martin: 2,
+  //   Edsger W. Dijkstra: 1
+  // }
+  const topAuthor = _.maxBy(_.keys(blogCounts), author => blogCounts[author])
+
+  // _.keys(blogCounts) -> gets an array of authors, e.g., ['Robert C. Martin', 'Edsger W. Dijkstra'].
+  // _.maxBy(..., callback) -> finds the author name whose count is highest, by passing each author to blogCounts[author].
+  // Final result: 'Robert C. Martin'
+
+  logger.info(`${blogCounts[topAuthor]} ${topAuthor}`);
+
+  return {
+    author: topAuthor,
+    blogs: blogCounts[topAuthor]
+  }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
