09b323cf8dd2f34eb4fa5bbe329780a4356257c8
Ujala Adil <ujadil99@gmail.com>
2025-07-09 01:46:00 +0300

    Ex: 4.8: Blog List Tests, step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    author: 'Brad Pitt',
+    title: 'How to make movies',
+    url: 'www.google.com',
+    likes: 3,
+  },
+  {
+    author: 'Lara Kroft',
+    title: 'What is so good in Tomb Raider?',
+    url: 'www.google.com',
+    likes: 2,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test.only('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,9 +1,23 @@
 const info = (...params) => {
-  console.log(...params)
+
+  if (process.env.NODE_ENV !== 'test') { 
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+
+  if (process.env.NODE_ENV !== 'test') { 
+    console.error(...params)
+  }
 }
 
-module.exports = { info, error }
+module.exports = {
+  info, error
+}
+
+
+//REASON BEHIND ADDING THE NODE_ENV CHECKS:
+//The middleware that outputs information about the HTTP requests is obstructing the test execution output. 
+// Modify the logger so that it does not print to the console in test mode.
+
