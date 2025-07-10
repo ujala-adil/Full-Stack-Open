0ed4d2565563ee50d4ab02ae272bf04cdc45e352
Ujala Adil <ujadil99@gmail.com>
2025-07-10 03:59:37 +0300

    Ex: 3.20*: Phonebook database, step 8-minlength

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -23,6 +23,7 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
     validate: {
       validator: function (v) {
         return /^\d{2}-\d{7}|\d{3}-\d{8}$/.test(v)
