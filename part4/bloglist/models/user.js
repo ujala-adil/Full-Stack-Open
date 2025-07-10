55645b347d5bcfacb661472c12193485f198a48d
Ujala Adil <ujadil99@gmail.com>
2025-07-10 03:52:18 +0300

    Ex: 4.15: Blog List Expansion, step 3

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  username: String,
   name: String,
   passwordHash: String,
   blogs: [
